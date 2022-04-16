0326f85fbfe370022976afc569da576c7fc85928
Yancy Vance <yancyvance@gmail.com>
2022-04-16 00:10:12 -0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -13,6 +13,6 @@ echo \
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
-newgrp docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+newgrp docker
